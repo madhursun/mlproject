5a3f2efc9b14363e11ec6c1abbcaec8f366322e0
madhursun <madhursun@gmail.com>
2026-02-18 16:38:18 +0530

    pkl created

--- src/logger.py
+++ src/logger.py
@@ -4,8 +4,8 @@ import datetime
 
 # Get project root (go 1 level up from src)
 PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+LOG_File = f'log_{datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.log'
 
-LOG_File = f'log_{datetime.datetime.now().strftime("%Y-%m-%d")}.log'
 
 logs_path = os.path.join(PROJECT_ROOT, "logs")
 os.makedirs(logs_path, exist_ok=True)
@@ -14,6 +14,8 @@ LOG_File_Path = os.path.join(logs_path, LOG_File)
 
 logging.basicConfig(
     filename=LOG_File_Path,
+    filemode='a',   # append instead of overwrite
     format='[%(asctime)s] %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
+
